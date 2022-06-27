36d2ecfc87d88895309d4d260601d7c559947fdf
KIMMUSIC <ldgyuuuuu@gmail.com>
2022-06-28 03:01:12 +0900

    add frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
-function App () {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    fetch('/api/hello')
-        .then(response => response.text())
-        .then(message => {
-          setMessage(message);
-        });
-    },[])
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AP from "./pages/AP";
+
+function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">{message}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-  )} export default App;
+    <Router>
+      <Switch>
+       <Route exact path="/">
+        <HomePage/>
+        </Route>
+          <Route exact path="/api/hello">
+              <AP/>
+          </Route>
+      </Switch>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderCompound from "../compounds/HeaderCompund";
+
+function HomePage() {
+  return (
+    <>
+      <HeaderCompound>
+      </HeaderCompound>
+    </>
+  );
+}
+
+export default HomePage;
