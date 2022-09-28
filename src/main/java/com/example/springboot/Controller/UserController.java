af219f3d34076e11d7be7fec55706b27d3972452
KIMMUSIC <ldgyuuuuu@gmail.com>
2022-09-29 01:56:44 +0900

    custom exception

--- src/main/java/com/example/springboot/Controller/UserController.java
+++ src/main/java/com/example/springboot/Controller/UserController.java
@@ -2,10 +2,13 @@ package com.example.springboot.Controller;
 
 import com.example.springboot.DTO.LoginResponseDto;
 import com.example.springboot.config.JwtTokenProvider;
+import com.example.springboot.exception.CustomException;
 import com.example.springboot.jpa.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +22,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.Optional;
+
+import static com.example.springboot.exception.ErrorCode.USER_NOT_FOUND;
 
 @Slf4j
 @RestController
@@ -36,13 +41,14 @@ public class UserController {
 
     // 로그인
     @PostMapping("/api/login")
-    public LoginResponseDto login(HttpServletResponse response, @RequestBody Map<String, String> user) {
+    public ResponseEntity<LoginResponseDto> login(HttpServletResponse response, @RequestBody Map<String, String> user) {
         log.info("user id = {} user password = {}", user.get("id"), user.get("password"));
         Dusers member = dusersRepository.findByUserId(user.get("id"));
 
 
         if(!passwordEncoder.matches(user.get("password"), member.getUserPassword())){
-            throw new IllegalArgumentException("비밀번호 오류");
+            //throw new IllegalArgumentException("비밀번호 오류");
+            throw new CustomException(USER_NOT_FOUND);
         }
 
 
@@ -65,13 +71,14 @@ public class UserController {
 
 
 
-        return loginResponseDto;
+        return new ResponseEntity<>(loginResponseDto, HttpStatus.OK);
+
 
     }
 
 
     @GetMapping("/reissue")
-    public String reissue(HttpServletResponse response, HttpServletRequest request){
+    public ResponseEntity<String> reissue(HttpServletResponse response, HttpServletRequest request){
 
         String token = jwtTokenProvider.resolveRefreshToken((HttpServletRequest) request, "Refresh");
         String Atoken = jwtTokenProvider.resolveToken((HttpServletRequest) request, "AUTHORIZATION");
@@ -94,15 +101,16 @@ public class UserController {
 
             response.addCookie(cookie);
 
-            return access;
+            return new ResponseEntity<>(access, HttpStatus.OK);
 
         }
-        return "access token expired";
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/silent-refresh")
     public String silent_refresh(HttpServletResponse response, HttpServletRequest request){
         String token = jwtTokenProvider.resolveRefreshToken((HttpServletRequest) request, "Refresh");
+        System.out.println(token);
         // 유효한 토큰인지 확인합니다.
         if (token != null && jwtTokenProvider.validateToken(token) == JwtTokenProvider.JwtCode.ACCESS) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
@@ -116,6 +124,7 @@ public class UserController {
 
             String newrefreshToken = jwtTokenProvider.createRefreshToken();
             refreshToken.changeToken(newrefreshToken);
+            refreshTokenRepository.save(refreshToken);
 
             Cookie cookie = new Cookie("Refresh", newrefreshToken);
             cookie.setPath("/");
---
+++ src/main/java/com/example/springboot/exception/CustomExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.springboot.exception;
+
+import org.springframework.http.ResponseEntity;
+import com.example.springboot.exception.CustomException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
+        return ErrorResponseEntity.toResponseEntity(e.getErrorCode());
+    }
+}
